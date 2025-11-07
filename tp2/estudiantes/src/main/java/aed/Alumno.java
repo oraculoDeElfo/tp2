550ae6cab011f11658adcafbb7d8bb5f5808c841
luji <aldanaylan@gmail.com>
2025-11-07 18:58:52 -0300

    agrego obtenerExamen

--- tp2/estudiantes/src/main/java/aed/Alumno.java
+++ tp2/estudiantes/src/main/java/aed/Alumno.java
@@ -33,6 +33,10 @@ public class Alumno implements Comparable<Alumno>{
         return this.id;
     }
 
+    public int[] obtenerExamen(){
+        return this.examen;
+    }
+
     private int obtenerHandle(){
         return this.handle;
     }
