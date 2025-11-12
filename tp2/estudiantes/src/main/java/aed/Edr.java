67531a227c2e524c0d5df069d349685af51cb8ae
Lucciano Rissso <lucciano.risso@gmail.com>
2025-11-12 18:12:48 -0300

    copiarse hecho

--- tp2/estudiantes/src/main/java/aed/Edr.java
+++ tp2/estudiantes/src/main/java/aed/Edr.java
@@ -27,40 +27,68 @@ public class Edr {
 
 //------------------------------------------------COPIARSE------------------------------------------------------------------------
 
-
-    private Alumno a_quien_copiar(int id){
-        return null;
+    private void copiarseAux(Alumno se_copia, int[] copiado_ex){
+        int[] se_copia_examen = se_copia.obtenerExamen();
+        int i = 0;
+            while (i<se_copia_examen.length || (copiado_ex[i] != -1 && se_copia_examen[i]==-1)){
+                i++;
+            }
+            se_copia.modificarExamen(i, copiado_ex[i]);
     }
 
     public void copiarse(int estudiante) {
         Alumno se_copia = this.alumnos.get(estudiante);
-        
+        int[] se_copia_examen = se_copia.obtenerExamen();
 
+        Alumno v1 = this.alumnos.get(estudiante + 1);
+        int[] v1_examen = v1.obtenerExamen();
+
+
+        Alumno v2 = this.alumnos.get(estudiante - 1);
+        int[] v2_examen = v2.obtenerExamen();
+
+        Alumno v3 = null;
+        if (this.ladoAula % 2 == 0){
+            v3 = this.alumnos.get(estudiante - (this.ladoAula/2));
+        }
+        else{
+            v3 = this.alumnos.get(estudiante - (this.ladoAula/2)+1);
+        }
+        int[] v3_examen = v3.obtenerExamen();
+        
         int contador_v1 = 0;
         int contador_v2 = 0;
         int contador_v3 = 0;
-
-
-
-        for (int i=0; i<examen_alumno.length; i++){
-            if (examen_alumno[i] == null){
-
-                if (examen_v1[i] != null){
+        for (int i=0; i<se_copia_examen.length; i++){
+            if (se_copia_examen[i] == -1){
+                if (v1_examen[i] != -1){
                     contador_v1 += 1;
                 }
-                if (examen_v2[i] != null){
+                if (v2_examen[i] != -1){
                     contador_v1 += 1;
                 }
-                if (examen_v2[i] != null){
+                if (v3_examen[i] != -1){
                     contador_v1 += 1;
                 }
             }
         }
 
-        if (contador_v1 >= )
-        Alumno v_a_copiar = a_quien_copiar(estudiante, );
+        if ((contador_v1 >= contador_v2 && contador_v2 >= contador_v3) || (contador_v1 >= contador_v3 && contador_v3 >= contador_v2)){
+            copiarseAux(se_copia, v1_examen);
+            return; 
+        }
+        if ((contador_v2 >= contador_v1 && contador_v1 >= contador_v3) || (contador_v2 >= contador_v3 && contador_v3 >= contador_v1)){
+            copiarseAux(se_copia, v2_examen);
+            return; 
+        }
+        if ((contador_v3 >= contador_v1 && contador_v1 >= contador_v2) || (contador_v3 >= contador_v2 && contador_v2 >= contador_v1)){
+            copiarseAux(se_copia, v3_examen);
+            return; 
+        }
+        else{
+            return;
+        }
     }
-
 
 //-----------------------------------------------RESOLVER----------------------------------------------------------------
 
